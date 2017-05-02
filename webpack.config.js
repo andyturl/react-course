935d12f02e552b53a96eed8fc1a908fb2d082659
Andrew Turley <andrew.turley@achilles.com>
2017-05-02 14:23:46 +0100

    Switch to SCSS

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
             openWeatherMap: 'app/api/openWeatherMap.jsx',
             About: 'app/components/About.jsx',
             Examples: 'app/components/Examples.jsx',
-            applicationStyles: 'app/styles/app.css'
+            applicationStyles: 'app/styles/app.scss'
         },
         extensions: ['', '.js', '.jsx']
     },
